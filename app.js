3f26646da086a5cb3e54b80dda8c519b06a88ed3
Julian Governale <julian@Batcave.local>
2017-08-07 20:19:31 -0400

    added jade template and stylus

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.use(express.static(__dirname + '/public'))
 
 
 app.get('/', function (req, res) {
-  res.end('Hi there!')
+  res.render('index',
+  { title : 'Home' }
+  )
 })
 app.listen(8080)
